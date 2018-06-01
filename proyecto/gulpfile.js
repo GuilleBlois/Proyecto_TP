434a2b626a2ff22421ba45de8030d86e7b572935
GuilleBlois <GuilleBlois@users.noreply.github.com>
2018-06-01 08:26:58 +0200

    Primera subida
    Pues eso...

---
+++ proyecto/gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var atImport = require('postcss-import');
+var mixins = require('postcss-mixins');
+var nested = require('postcss-nested');
+var simplevars = require('postcss-simple-vars');
+var autoprefixer = require('autoprefixer');
+
+gulp.task('css',function(){
+    var processors=[
+        atImport,
+        mixins,
+        nested,
+        simplevars,
+        autoprefixer
+    ];
+    return gulp.src('./src/style.css')
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./recursos'));
+});
+
+gulp.task('watch',function(){
+	gulp.watch('src/**/*.css',['css']);
+});
+
+gulp.task('default',['css','watch']);
